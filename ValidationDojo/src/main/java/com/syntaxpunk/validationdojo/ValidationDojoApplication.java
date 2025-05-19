6a52b963bb4bf9fe835024b2d65ce283b141b215
syntax-punk <david.jaeren@gmail.com>
2025-05-19 21:18:45 +0200

    Added java stuff

---
+++ ValidationDojo/src/main/java/com/syntaxpunk/validationdojo/ValidationDojoApplication.java
@@ -0,0 +1,13 @@
+package com.syntaxpunk.validationdojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationDojoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValidationDojoApplication.class, args);
+    }
+
+}
---
+++ ValidationDojo/src/test/java/com/syntaxpunk/validationdojo/ValidationDojoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syntaxpunk.validationdojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationDojoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
