275dd5b3ca1efcfc51d392308b78543c554df4d5
syntax-punk <david.jaeren@gmail.com>
2025-05-21 12:27:00 +0200

    Backend validation is done

--- ValidationDojo/src/main/java/com/syntaxpunk/validationdojo/users/dtos/CreateUserDto.java
+++ ValidationDojo/src/main/java/com/syntaxpunk/validationdojo/users/dtos/CreateUserDto.java
@@ -1,5 +1,6 @@
 package com.syntaxpunk.validationdojo.users.dtos;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.time.LocalDate;
@@ -7,12 +8,27 @@ import java.time.LocalDate;
 @Data
 @NoArgsConstructor
 public class CreateUserDto {
+    @NotBlank
+    @Pattern(regexp ="^\\p{L}+([\\s\\-']?\\p{L}+)*$")
     private String firstName;
+
+    @NotBlank
     private String lastName;
+
+    @NotBlank
     private LocalDate dob;
+
+    @NotBlank
     private String gender;
+
+    @NotBlank
     private String username;
+
+    @Email
     private String email;
+
     private String phone;
+
+    @Size(max = 20)
     private String bio;
 }
---
+++ ValidationDojo/src/main/java/com/syntaxpunk/validationdojo/validation/ValidationExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.syntaxpunk.validationdojo.validation;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ValidationExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            String field = error.getField();
+            String message = error.getDefaultMessage();
+            errors.put(field, message);
+        });
+
+        ErrorResponse response = new ErrorResponse("Provided data is not valid", errors);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
